7b2d4c6ac438ec0a58295880e6a10232f308bfed
Hayri <hello@hoshilab.com>
2014-09-22 18:52:55 +0200

    added get_search_result method

--- with_rspec/series_master.rb
+++ with_rspec/series_master.rb
@@ -4,6 +4,10 @@ class SeriesMaster
     Imdb::Search.new(argument).movies.first.rating
   end
 
+  def get_search_result(argument)
+    Imdb::Search.new(argument).movies.size
+  end
+
   def best_serie(argument)
 
     argument.map do |element|
@@ -18,16 +22,23 @@ end
 
 series_master = SeriesMaster.new
 series_master.best_serie(["The Wire", "The Office", "Breaking Bad"])
+series_master.get_search_result("Breaking Bad")
 
 describe SeriesMaster do 
+  before do 
+    @series_master = SeriesMaster.new
+  end
+
   describe "#best_serie" do
-    before do 
-      @series_master = SeriesMaster.new
-    end
 
     it "should sort the array by the rating best first bad last" do
       expect(@series_master.best_serie(["The Wire","The Office", "Breaking Bad"])).to eql("Breaking Bad")
     end
-
   end  
+
+  describe "#search_for_number_of_results" do
+    it "should get all the result of the search" do
+      expect(@series_master.get_search_result("Cat")).to eql(202)
+    end
+  end   
 end
