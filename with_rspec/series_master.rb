f628b2f0fff9fcea4f0c216213835fc77c024eaf
Hayri <hello@hoshilab.com>
2014-09-22 17:58:57 +0200

    added the sort method for best serie

--- with_rspec/series_master.rb
+++ with_rspec/series_master.rb
@@ -1,13 +1,23 @@
+require 'imdb'
 class SeriesMaster
+  def get_rating?(argument)
+    Imdb::Search.new(argument).movies.first.rating
+  end
 
   def best_serie(argument)
-    ["The Wire","The Office"]
+
+    argument.map do |element|
+      [element, get_rating?(element)]
+    end.sort do |rating1, rating2|
+      rating1 <=> rating2
+    end.first.first
+
   end
 
 end
 
 series_master = SeriesMaster.new
-series_master.best_serie(["The Wire", "The Office"])
+series_master.best_serie(["The Wire", "The Office", "Breaking Bad"])
 
 describe SeriesMaster do 
   describe "#best_serie" do
@@ -15,8 +25,8 @@ describe SeriesMaster do
       @series_master = SeriesMaster.new
     end
 
-    it "should get multiple elemenents of array" do
-      expect(@series_master.best_serie(["The Wire","The Office"])).to eql(["The Wire","The Office"])
+    it "should sort the array by the rating best first bad last" do
+      expect(@series_master.best_serie(["The Wire","The Office", "Breaking Bad"])).to eql("Breaking Bad")
     end
 
   end  
